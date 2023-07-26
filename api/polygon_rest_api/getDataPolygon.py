cc3cdaf39265b099cea37dd09cb8ec7a7e6ed2f6
Oj <ojudz@example.com>
2023-07-26 15:09:40 +0800

    add counter and weekday

--- api/polygon_rest_api/getDataPolygon.py
+++ api/polygon_rest_api/getDataPolygon.py
@@ -35,45 +35,48 @@ class CryptoData():
         # Initialize empty dataframe
         result_df = pd.DataFrame()
 
+        # Set counter to pause if requests reaches 5
+        counter = 0
         for i in range(0, dt_diff):
+            counter = counter + 1
+            if counter == 5:
+                time.sleep(65)
+                counter = 0
+
             dt_1 = dt_start + pd.Timedelta(days = i)
             date = dt_1.strftime("%Y-%m-%d")
 
-            url = f"{self.api_url}/v2/aggs/grouped/locale/global/market/crypto/{date}?adjusted=true&apiKey={config.api_key}"
-            request_data = requests.get(url)
+            if dt_1.weekday() < 5:
+                url = f"{self.api_url}/v2/aggs/grouped/locale/global/market/crypto/{date}?adjusted=true&apiKey={config.api_key}"
+                request_data = requests.get(url)
+            
+                crypto_df = pd.DataFrame(request_data.json()['results'])
+                crypto_df = crypto_df.rename(columns={"T": "Crypto Pair",
+                                                      "v": "Volume",
+                                                      "vw": "Volume Weighted Ave Price",
+                                                       "o": "Open",
+                                                       "c": "Close",
+                                                       "h": "High",
+                                                       "l": "Low",
+                                                       "t": "Timestamp (Unix Msec)",
+                                                       "n": "Num of Transactions"}, errors="raise")
 
-            # if API calls exceed 5 where response!=200, wait 65 sec before proceeding
-            #if request_data.status_code == 200:
-            #    continue
-            #else:
-            #    time.sleep(65)
+                data_df = pd.concat([pd.DataFrame([date] * len(crypto_df), columns=["Date"]), crypto_df], axis=1)
             
-            crypto_df = pd.DataFrame(request_data.json()['results'])
-            
-            crypto_df = crypto_df.rename(columns={"T": "Crypto Pair",
-                                                  "v": "Volume",
-                                                  "vw": "Volume Weighted Ave Price",
-                                                  "o": "Open",
-                                                  "c": "Close",
-                                                  "h": "High",
-                                                  "l": "Low",
-                                                  "t": "Timestamp (Unix Msec)",
-                                                  "n": "Num of Transactions"}, errors="raise")
-
-            data_df = pd.concat([pd.DataFrame([date] * len(crypto_df), columns=["Date"]), 
-                             crypto_df], axis=1)
-            
-            result_df = pd.concat([result_df, data_df], axis=0)
+                result_df = pd.concat([result_df, data_df], axis=0)
+            else:
+                pass
         
         # Save to csv
         result_df.to_csv(f"{self.project_dir}\ohlc_data\{self.date_frm}_{self.date_to}.csv", index=False)
         
         return result_df
+        
     
 
 if __name__ == "__main__":
     pair = ["TRX", "USD"]
-    date_range = ["2023-07-17", "2023-07-20"]
+    date_range = ["2023-01-01", "2023-07-21"]
     
     tronData = CryptoData(pair, date_range)
     data = tronData.ohlc_allCryptoMarket()
