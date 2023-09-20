64f0732f9140b3fb7160bc65b26334c94418164c
Oj <ojudz@example.com>
2023-09-20 11:54:45 +0800

    update rates&spreads func

--- parsing_documents/parsePDFs01/parsepdf01.py
+++ parsing_documents/parsePDFs01/parsepdf01.py
@@ -18,12 +18,12 @@ import numpy as np
 
 class pdfParse():
 
-    def __init__(self, report_folder, output_folder, filename):
+    def __init__(self):
         parent_dir = Path(__file__).parents[0]
         sys.path.append(parent_dir)
 
-        self.filename = os.path.join(parent_dir, report_folder, filename)
-        self.output = os.path.join(parent_dir, output_folder)
+        self.filename = os.path.join(parent_dir, "reports", filename)
+        self.output = os.path.join(parent_dir, "output")
 
 
     def readPdf(self, box, pg, strm):
@@ -41,19 +41,23 @@ class pdfParse():
         return df
 
     def getIndex(self):
+        """ Gets the index of each asset type"""
         df1 = self.readPdf([2, 0, 27, 11], 3, True)[0]
-        assetCol1 = df1.iloc[:,0]
-        asset = ["EQUITIES", "FIXED INCOME", "OTHER"]
+        df2 = self.readPdf([2, 11, 13, 22], 3, True)[0]
+        assetCol1, assetCol2 = df1.iloc[:,0], df2.iloc[:,0]
+        asset = ["EQUITIES", "FIXED INCOME", "OTHER", "RATES", "SPREADS"]
         idx = [assetCol1[assetCol1 == asset[0]].index[0], 
                assetCol1[assetCol1 == asset[1]].index[0],
-               assetCol1[assetCol1 == asset[2]].index[0]]
+               assetCol1[assetCol1 == asset[2]].index[0],
+               assetCol2[assetCol2 == asset[3]].index[0],
+               assetCol2[assetCol2 == asset[4]].index[0]]
         return idx
 
     def indexReturns(self):
         """
           Parse the Index Returns table (page 3 of the pdf)
             :input   None, Input df is called from readPdf function
-            :output  data - Index Returns manipulated table output as dataframe
+            :output  data - Index Returns output as dataframe
         """
         df = self.readPdf([2, 0, 20, 11], 3, True)[0]
         idx = self.getIndex()
@@ -70,7 +74,7 @@ class pdfParse():
             temp2_df = pd.concat([temp1_df, indexRet_df], axis=1, ignore_index=True)
             data = pd.concat([data, temp2_df], ignore_index=True)       
 
-        data.columns = ["Type", "Asset Type", "Index", "1 week", "MTD", "QTD", "YTD"]
+        data.columns = ["Type", "Asset Type", "Index"] + df.iloc[0, 1:5].values.tolist()
         
         return data
 
@@ -79,7 +83,7 @@ class pdfParse():
         """
           Parse the Commodities table (page 3 of the pdf)
             :input   None, Input df is called from readPdf function
-            :output  data - Commodities Returns manipulated table output as dataframe
+            :output  data - Commodities Returns output as dataframe
         """
         df = self.readPdf([20, 0, 23, 11], 3, True)[0]
         commodities_df = df.iloc[1: len(df)].reset_index(drop=True)
@@ -95,7 +99,7 @@ class pdfParse():
         """
           Parse the Currencies table (page 3 of the pdf)
             :input   None, Input df is called from readPdf function
-            :output  data - Currencies Returns manipulated table output as dataframe
+            :output  data - Currencies Returns output as dataframe
         """
         df = self.readPdf([23, 0, 27, 11], 3, True)[0]
         fx_df = df.iloc[1: len(df)].reset_index(drop=True)
@@ -110,61 +114,53 @@ class pdfParse():
     def ratesSpreads(self):
         """
           Parse the Rates & Spreads table (page 3 of the pdf)
-            :input
-              None. Input df is called from readPdf function
-            :output
-              data - rates or spreads manipulated table output as dataframe
+            :input   None, Input df is called from readPdf function
+            :output  data - Rates or Spreads output as dataframe
         """
         df = self.readPdf([2, 11, 13, 22], 3, True)[0]
-        marketType = df.columns.values[0]
-        prd = df.iloc[0, :]
-        period = [prd[i] for i in range(0, len(prd)) if pd.isnull(prd[i]) == False]
-        rtType = df.iloc[:, 0]
-        rateType = [rtType[i] for i in range(0, len(rtType)) if
-                     pd.isnull(df.iloc[:, 1][i]) == True]
-        idx = df[df.iloc[:, 0].isin(rateType)].index
-
+        idx = self.getIndex()[-2:]
+        asset = ["RATES", "SPREADS"]
+        
         data = pd.DataFrame()
-        for i in range(0, len(idx)):
-            if i < len(idx) - 1:
-                temp1 = df[idx[i] + 1: idx[i + 1]].reset_index(drop=True)
-            else:
-                temp1 = df[idx[i] + 1: len(df)].reset_index(drop=True)
-            temp1 = pd.concat([pd.DataFrame([marketType] * len(temp1)),
-                               pd.DataFrame([rateType[i]] * len(temp1)),
-                               temp1], axis=1, ignore_index=True)
-            data = data.append(temp1, ignore_index=True)
+        for i in range(0, 2):
+            if i == 1: idx_n = len(df)
+            else: idx_n = idx[i + 1] 
 
-        data.columns = ["Type", "Rate or Spread", "Items", period[0], period[1], period[2], period[3]]
+            ratesSpreads_df = df.iloc[idx[i] + 1: idx_n, :].reset_index(drop=True)
+            temp1_df = pd.DataFrame({0: ["Rates & Spreads"] * len(ratesSpreads_df), 
+                                     1: [asset[i]] * len(ratesSpreads_df)})
+            temp2_df = pd.concat([temp1_df, ratesSpreads_df], axis=1, ignore_index=True)
+            data = pd.concat([data, temp2_df], ignore_index=True)       
+
+        data.columns = ["Asset", "Rates or Spreads", "Asset Name"] + df.iloc[0, 1:5].values.tolist()   
+
         return data
 
 
-    def weeklyMarketRecap(self):
+    def weeklyMarketRecap(self, filename):
         """
           Saves output as an xlsx
-            :input
-              None. Inputs are from the multiple asset classs function
-            :output
-              outputs an xls file
+            :input   None. Inputs are from the multiple asset classs function
+            :output  Outputs as xls file
         """
+        print(f"Parsing {filename}...")
         idxRet = self.indexReturns()
         comm = self.commodities()
         curr = self.currencies()
         rtSp = self.ratesSpreads()
 
-        with pd.ExcelWriter(self.output) as writer:
+        output_file = os.path.join(self.output, filename[:-4] + '.xlsx')
+        with pd.ExcelWriter(output_file) as writer:
             idxRet.to_excel(writer, sheet_name="index_returns", index=False)
             comm.to_excel(writer, sheet_name="commodities", index=False)
             curr.to_excel(writer, sheet_name="currencies", index=False)
             rtSp.to_excel(writer, sheet_name="rates_spreads", index=False)
 
+        
 
 if __name__ == '__main__':
-    
-    report_folder = "reports"
-    output_folder = "output"
     filename = r"GSAM_Market_Monitor_081222.pdf"
 
-    convert = pdfParse(report_folder, output_folder, filename)
-    data = convert.currencies()
-    print(data)
+    convert = pdfParse()
+    data = convert.weeklyMarketRecap(filename)
+    print("Done converting data!")
