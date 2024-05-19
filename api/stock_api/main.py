867f3f328509dd1a64e93f74dde0e4a96ab8b5b6
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-05-20 00:06:56 +0800

    add main.py

---
+++ api/stock_api/main.py
@@ -0,0 +1,9 @@
+"""
+    Author: Ojelle Rogero
+    Email: ojelle.rogero@gmail.com
+    Initial version: May 20, 2023
+    Completed on: August 9, 2021
+
+    Getting data from PseAPI using requests.
+
+"""
