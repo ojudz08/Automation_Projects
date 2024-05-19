abf83430230ec4bbbaa64065d0aa1d40390259df
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-05-20 00:40:25 +0800

    update repos

--- api/stock_api/main.py
+++
@@ -1,9 +0,0 @@
-"""
-    Author: Ojelle Rogero
-    Email: ojelle.rogero@gmail.com
-    Initial version: May 20, 2023
-    Completed on: August 9, 2021
-
-    Getting data from PseAPI using requests.
-
-"""
