edff05b832ae463352dcc5038283390498e8ec42
Ojelle Rogero <ojelle.rogero@gmail.com>
2024-05-23 06:33:49 +0800

    update scripts

--- parsing_documents/parsePDFs01/run.bat
+++ parsing_documents/parsePDFs01/run.bat
@@ -1,3 +1,3 @@
 python -m pip install -r requirements.txt
-pyinstaller main.py --onefile --name pdf_parser
-python move_exe.py
+pyinstaller main.py --clean --onefile --name pdf_parser
+python scripts/file_transfer.py
